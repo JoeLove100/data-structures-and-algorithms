b79cb4ada68cc7609a1533c44994af52286e68bc
Joe L <joelove100@gmail.com>
2020-01-08 21:30:26 +0000

    Refactor fib number method for generator
    Refactor the method so we have an intermediate method which gets the nth
    generator value - we can then reuse this code in the next problem and
    just change the geneartor

--- algorithmic_warm_up/fibonacci_numbers.py
+++ algorithmic_warm_up/fibonacci_numbers.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Any
 
 
 def _fib_number_generator() -> Generator[int, None, None]:
@@ -23,34 +23,25 @@ def _fib_number_generator() -> Generator[int, None, None]:
         n_1 = n
 
 
-def get_nth_fib_number(n: int) -> int:
+def get_nth_generator_value(n: int,
+                            g: Generator) -> Any:
+    """
+    return the nth value from a generator
+    """
+
+    nth_value = next(x for i, x in enumerate(g) if i == n)
+    return nth_value
+
+
+def get_nth_fib_number(n):
     """
     get the nth fibonacci number using
-    a faster iterative method
+    a more efficient iterative method
     """
 
-    if n <= 1:
-        return n
-    else:
-        fib_generator = _fib_number_generator()
-
-        for _ in range(n):
-            next(fib_generator)
-
-        return next(fib_generator)
-
-
-def calc_fib_slow(n: int) -> int:
-    """
-    get the nth fibonacci number using a
-    recursive method with exponential time
-    complexity
-    """
-
-    if n <= 1:
-        return n
-
-    return calc_fib_slow(n - 1) + calc_fib_slow(n - 2)
+    g = _fib_number_generator()
+    fib_number = get_nth_generator_value(n, g)
+    return fib_number
 
 
 if __name__ == "__main__":
