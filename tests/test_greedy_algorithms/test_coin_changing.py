34f289354c6d6bf1f93ce20534fea5277cde0fee
Joe L <joelove100@gmail.com>
2020-01-11 16:01:57 +0000

    Add coin changing function
    Add function to get min number of coins needed for given value of change
    with some accompanying tests

---
+++ tests/test_greedy_algorithms/test_coin_changing.py
@@ -0,0 +1,41 @@
+import unittest
+from greedy_algorithms.coin_changing import get_min_number_of_coins
+
+
+class TestCoinChanging(unittest.TestCase):
+
+    def test_get_min_number_of_coins_multiple_of_max(self):
+        # arrange
+
+        n = 80
+        coin_vals = [8, 4, 3, 1]
+
+        # act
+        result = get_min_number_of_coins(n, coin_vals)
+
+        # assert
+        self.assertEqual(result, 8)
+
+    def test_get_min_number_of_coins_ones_only(self):
+        # arrange
+
+        n = 17
+        coin_vals = [1]
+
+        # act
+        result = get_min_number_of_coins(n,  coin_vals)
+
+        # assert
+        self.assertEqual(result, 17)
+
+    def test_get_min_number_of_coins(self):
+        # arrange
+
+        n = 28
+        coin_vals = [10, 5, 1]
+
+        # act
+        result = get_min_number_of_coins(n, coin_vals)
+
+        # assert
+        self.assertEqual(result, 6)
